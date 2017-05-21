e65510734768ad29b2d3d1e7e45a5e22e67540c7
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-21 13:40:21 +0200

    more tests

--- tests/test_quickhull.py
+++ tests/test_quickhull.py
@@ -3,7 +3,7 @@ import QuickHull
 
 
 class TestCase(unittest.TestCase):
-    def test_quickhull(self):
+    def test_quickhull_box(self):
         points = [
             (0, 0, 0),
             (0, 0, 1),
@@ -16,9 +16,68 @@ class TestCase(unittest.TestCase):
         ]
 
         vertices, triangles = QuickHull.quick_hull(points)
-        
+
         self.assertEqual(set(vertices), set(points))
         self.assertEqual(len(triangles), 12)
+
+    def test_quickhull_tetrahedron(self):
+        points = [
+            (0, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 0),
+        ]
+
+        points_inside = [
+            (0.1, 0.1, 0.1),
+            (0.2, 0.1, 0.4),
+            (0.1, 0.4, 0.2),
+            (0.3, 0.1, 0.2),
+        ]
+
+        test_points = points[0::2] + points_inside + points[1::2]
+        vertices, triangles = QuickHull.quick_hull(test_points)
+
+        self.assertEqual(set(vertices), set(points))
+        self.assertEqual(len(triangles), 4)
+
+    def test_quickhull_duplicates(self):
+        points = [
+            (0, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 0),
+        ]
+
+        test_points = points * 10
+        vertices, triangles = QuickHull.quick_hull(test_points)
+
+        self.assertEqual(set(vertices), set(points))
+        self.assertEqual(len(triangles), 4)
+
+    def test_quickhull_errors_1(self):
+        points = [
+            (0, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 0),
+            'something else',
+        ]
+
+        with self.assertRaises(Exception):
+            vertices, triangles = QuickHull.quick_hull(points)
+
+    def test_quickhull_errors_2(self):
+        points = [
+            (0, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+            (1, 0, 0),
+            ('a', 'b', 'c'),
+        ]
+
+        with self.assertRaises(Exception):
+            vertices, triangles = QuickHull.quick_hull(points)
 
 
 if __name__ == '__main__':

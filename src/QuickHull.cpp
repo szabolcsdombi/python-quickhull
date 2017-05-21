23c2d9213ea3c74d04a9b5a2b147d46215df3b2c
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-21 13:04:53 +0200

    pass points and get result

--- src/QuickHull.cpp
+++ src/QuickHull.cpp
@@ -5,14 +5,60 @@
 using namespace quickhull;
 
 PyObject * meth_quick_hull(PyObject * self, PyObject * args) {
+	PyObject * points;
+	int ccw;
+
+	int args_ok = PyArg_ParseTuple(
+		args,
+		"Op",
+		&points,
+		&ccw
+	);
+
 	QuickHull<double> qh;
 	std::vector<Vector3<double>> pointCloud;
 
-	auto hull = qh.getConvexHull(pointCloud, true, false);
+	int pointsSize = PyTuple_GET_SIZE(points);
+	pointCloud.reserve(pointsSize);
+
+	for (int i = 0; i < pointsSize; ++i) {
+		PyObject * xyz = PyTuple_GET_ITEM(points, i);
+		if (Py_TYPE(xyz) != &PyTuple_Type) {
+			return 0;
+		}
+
+		double x = PyFloat_AsDouble(PyTuple_GET_ITEM(xyz, 0));
+		double y = PyFloat_AsDouble(PyTuple_GET_ITEM(xyz, 1));
+		double z = PyFloat_AsDouble(PyTuple_GET_ITEM(xyz, 2));
+		pointCloud.push_back(Vector3<double>(x, y, z));
+	}
+
+	auto hull = qh.getConvexHull(pointCloud, ccw ? true : false, false);
+
 	auto indexBuffer = hull.getIndexBuffer();
 	auto vertexBuffer = hull.getVertexBuffer();
 
-	return 0;
+	int indexBufferSize = indexBuffer.size();
+	int vertexBufferSize = vertexBuffer.size();
+
+	PyObject * vertexList = PyList_New(vertexBufferSize);
+
+	for (int i = 0; i < vertexBufferSize; ++i) {
+		PyObject * x = PyFloat_FromDouble(vertexBuffer[i].x);
+		PyObject * y = PyFloat_FromDouble(vertexBuffer[i].y);
+		PyObject * z = PyFloat_FromDouble(vertexBuffer[i].z);
+		PyObject * vertex = PyTuple_Pack(3, x, y, z);
+		PyList_SET_ITEM(vertexList, i, vertex);
+	}
+
+	PyObject * indexList = PyList_New(indexBufferSize);
+
+	for (int i = 0; i < indexBufferSize; ++i) {
+		PyObject * index = PyLong_FromLong(indexBuffer[i]);
+		PyList_SET_ITEM(indexList, i, index);
+	}
+
+	return PyTuple_Pack(2, vertexList, indexList);
 }
 
 PyMethodDef methods[] = {

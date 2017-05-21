97a4b57b1cfdd4134c0572196e89e3998a8ab352
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-21 13:51:24 +0200

    example

--- QuickHull/__init__.py
+++ QuickHull/__init__.py
@@ -19,13 +19,51 @@ if False:
         '''
             hull_quick_hull
         '''
-    
+
     hull.quick_hull = hull_quick_hull
 
 
 def quick_hull(points, ccw=True):
     '''
-        Create a hull.
+        Create a convex hull of 3D points.
+
+        Args:
+            points (tuple): Points as 3-tuples: (x, y, z)
+            ccw (bool): The triangles orientation, counter clockwise by default.
+
+        Returns:
+            tuple: vertices and triangles
+
+        Example:
+            Example input and output::
+                points = [
+                    (0.0, 0.0, 0.0),
+                    (0.0, 1.0, 0.0),
+                    (0.1, 0.1, 0.1),
+                    (0.2, 0.1, 0.4),
+                    (0.1, 0.4, 0.2),
+                    (0.3, 0.1, 0.2),
+                    (0.0, 0.0, 1.0),
+                    (1.0, 0.0, 0.0),
+                ]
+
+                vertices, triangles = QuickHull.quick_hull(points)
+
+                # The output will be:
+
+                vertices = [
+                    (0.0, 1.0, 0.0),
+                    (0.0, 0.0, 1.0),
+                    (1.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                ]
+
+                triangles = [
+                    (0, 2, 1),
+                    (1, 2, 3),
+                    (2, 0, 3),
+                    (0, 1, 3),
+                ]
     '''
 
     return hull.quick_hull(tuple(points), ccw)

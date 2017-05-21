dbcbf65c65e95d565ba52454be2077bec2dbf629
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-21 12:37:28 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+
+QuickHull = Extension(
+	'QuickHull.hull',
+	sources = [
+		'src/QuickHull.cpp',
+	]
+)
+
+setup(
+	name = 'QuickHull',
+	version = '0.9.0',
+	description = 'QuickHull',
+	url = 'https://github.com/cprogrammer1994/QuickHull',
+	author = 'Szabolcs Dombi',
+	author_email = 'cprogrammer1994@gmail.com',
+	license = 'MIT',
+	packages = ['QuickHull'],
+	ext_modules = [QuickHull],
+	platforms = ['win32', 'win64']
+)

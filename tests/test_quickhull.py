a448a8aec553cb3a157e7fc0d1fe59adf1432a03
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-21 13:19:42 +0200

    test content

--- tests/test_quickhull.py
+++ tests/test_quickhull.py
@@ -4,7 +4,18 @@ import QuickHull
 
 class TestCase(unittest.TestCase):
     def test_quickhull(self):
-        pass
+        points = [
+            (0, 0, 0),
+            (0, 0, 1),
+            (0, 1, 0),
+            (0, 1, 1),
+            (1, 0, 0),
+            (1, 0, 1),
+            (1, 1, 0),
+            (1, 1, 1),
+        ]
+
+        vert, idx = QuickHull.quick_hull(points)
 
 
 if __name__ == '__main__':
